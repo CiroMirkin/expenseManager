c69bd881c9e1c2f8672391766a290705c3841a04
C.Mirkin <ciromirkin@gmail.com>
2022-12-15 08:56:06 -0300

    cambiado: nombres y pequeñas cosas de lugar.

--- src/view.js
+++ src/view.js
@@ -1,40 +1,38 @@
-const Calculo = require('./calculo')
+import Calculo from './calculo.js'
 
 class FormatObjectsToHTMLElements {
     fortmatExpenses(expenses) {
-        return expenses.map(expense => `<li class="list-group-item d-flex justify-content-between align-items-center"><div class="text-truncate">${expense.comment}</div> <span>$${expense.amount}</span></li>`)
+        return expenses.map(expense => `<li class="list-group-item d-flex justify-content-between align-items-center"><div class="text-truncate">${expense.comment}</div> <span>$${expense.amount}</span></li>`).join('')
     }
     formatIncome(incomes) {
-        return incomes.map(income => `<li class="list-group-item d-flex justify-content-between align-items-center"><div class="text-truncate">${income.comment}</div> <span>$${income.amount}</span></li>`)
+        return incomes.map(income => `<li class="list-group-item d-flex justify-content-between align-items-center"><div class="text-truncate">${income.comment}</div> <span>$${income.amount}</span></li>`).join('')
     }
 }
 
 export default class View {
     constructor() {
-        this.expensesListElement = document.getElementById('expenses-list')
-        this.incomeListElement = document.getElementById('income-list')
         this.calculo = new Calculo() 
         this.formatObjectsToHTMLElements = new FormatObjectsToHTMLElements()
 
     }
     showExpenses(expenses) {
-        this.expensesListElement.innerHTML = this.formatObjectsToHTMLElements.fortmatExpenses(expenses)
+        const expensesListElement = document.getElementById('expenses-list')
+        expensesListElement.innerHTML = this.formatObjectsToHTMLElements.fortmatExpenses(expenses)
     }
-    showIncome(income) {
-        this.incomeListElement.innerHTML = this.formatObjectsToHTMLElements.formatIncome(income)
+    showIncomes(income) {
+        const incomeListElement = document.getElementById('income-list')
+        incomeListElement.innerHTML = this.formatObjectsToHTMLElements.formatIncome(income)
     }
     showTotalExpenses(expenses) {
         const totalExpensesElement = document.getElementById('total-expenses')
         totalExpensesElement.innerText = this.calculo.calculateTotalExpenses(expenses)
     }
-    showTotalExpenses(income) {
+    showTotalIncome(income) {
         const totalIncomeElement = document.getElementById('total-income')
         totalIncomeElement.innerText = this.calculo.calculateTotalIncomes(income)
     }
     showTotalAmount({ income, expenses }){
         const totalAmountElement = document.getElementById('total')
-        totalAmountElement.innerText = this.calculo.calculateTotal().total
+        totalAmountElement.innerText = this.calculo.calculateTotal({ income, expenses }).total
     }
 }
-
-module.exports = FormatObjectsToHTMLElements
