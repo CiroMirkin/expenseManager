a0665aecd385742499949b3e2acb2d9555eba29d
C.Mirkin <ciromirkin@gmail.com>
2022-12-15 08:29:01 -0300

    movido: clases relacionadas a la vista a un archivo aparte.

--- src/index.js
+++ src/index.js
@@ -24,28 +24,3 @@ const accounts = {
 
 }
 
-class FormatObjectsToHTMLElements {
-    fortmatExpenses(expenses) {
-        return expenses.map(expense => `<li class="list-group-item d-flex justify-content-between align-items-center"><div class="text-truncate">${expense.comment}</div> <span>$${expense.amount}</span></li>`)
-    }
-    formatIncome(incomes) {
-        return incomes.map(income => `<li class="list-group-item d-flex justify-content-between align-items-center"><div class="text-truncate">${income.comment}</div> <span>$${income.amount}</span></li>`)
-    }
-}
-
-class View {
-    constructor() {
-        this.expensesListElement = document.getElementById('expenses-list')
-        this.incomeListElement = document.getElementById('income-list')
-        this.formatObjectsToHTMLElements = new FormatObjectsToHTMLElements()
-
-    }
-    showExpenses(expenses) {
-        this.expensesListElement.innerHTML = this.formatObjectsToHTMLElements.fortmatExpenses(expenses)
-    }
-    showIncome(income) {
-        this.incomeListElement.innerHTML = this.formatObjectsToHTMLElements.formatIncome(income)
-    }
-}
-
-module.exports = FormatObjectsToHTMLElemCents
