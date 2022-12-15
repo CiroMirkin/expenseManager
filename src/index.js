40f7c710b4911a2b84f3950c7cc69b0594717325
C.Mirkin <ciromirkin@gmail.com>
2022-12-15 08:55:16 -0300

    agregado: clase manager

--- src/index.js
+++ src/index.js
@@ -1,26 +1,48 @@
 "use strict"
 
-const accounts = {
-    BNA: {
-        expenses: [
-            {
-                account: 'BNA',
-                categorie: 'super',
-                comment: 'algo para tomar',
-                amount: 300,
-                day: '13 December 2022'
+import View from "./view.js"
+
+class Manager {
+    constructor() {
+        this.accounts = {
+            BNA: {
+                expenses: [
+                    {
+                        account: 'BNA',
+                        categorie: 'Varios',
+                        comment: 'Cable USB',
+                        amount: 1500,
+                        day: '17 December 2022'
+                    }
+                ],
+                income: [
+                    {
+                        account: 'BNA',
+                        categorie: 'progresar',
+                        comment: 'ProgresAr',
+                        amount: 2050,
+                        day: '13 Octover 2022'
+                    },
+                    {
+                        account: 'BNA',
+                        categorie: 'super',
+                        comment: 'progresar',
+                        amount: 2050,
+                        day: '13 December 2022'
+                    },
+                ]
             }
-        ],
-        income: [
-            {
-                account: 'BNA',
-                categorie: 'super',
-                comment: 'algo para comer',
-                amount: 150,
-                day: '13 December 2022'
-            }
-        ]
+        
+        }
+        
+        this.view = new View()
     }
-
+    updateView(account) {
+        const { expenses, income } = this.accounts[account]
+        this.view.showExpenses(expenses)
+        this.view.showIncomes(income)
+        this.view.showTotalExpenses(expenses)
+        this.view.showTotalIncome(income)
+        this.view.showTotalAmount({ expenses, income })
+    }
 }
-
