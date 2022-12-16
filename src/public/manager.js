9522ec41914f6232f98aa2c25aa67f4935292e1a
C.Mirkin <ciromirkin@gmail.com>
2022-12-16 08:16:10 -0300

    agregado: metodos que actualizan la informacion de las cuentas agregando los nuevos ingresos/gastos.

--- src/public/manager.js
+++ src/public/manager.js
@@ -4,36 +4,14 @@ class Manager {
     constructor() {
         this.accounts = {
             BNA: {
-                expenses: [
-                    {
-                        account: 'BNA',
-                        categorie: 'Varios',
-                        comment: 'Cable USB',
-                        amount: 1500,
-                        day: '17 December 2022'
-                    }
-                ],
-                income: [
-                    {
-                        account: 'BNA',
-                        categorie: 'progresar',
-                        comment: 'ProgresAr',
-                        amount: 2050,
-                        day: '13 Octover 2022'
-                    },
-                    {
-                        account: 'BNA',
-                        categorie: 'super',
-                        comment: 'progresar',
-                        amount: 2050,
-                        day: '13 December 2022'
-                    },
-                ]
+                expenses: [],
+                income: []
             }
         
         }
         
         this.view = new View()
+        this.updateView(Object.keys(this.accounts).at(0))
     }
     updateView(account) {
         const { expenses, income } = this.accounts[account]
@@ -43,7 +21,15 @@ class Manager {
         this.view.showTotalIncome(income)
         this.view.showTotalAmount({ expenses, income })
     }
+    addNewIncome(income){
+        this.accounts[income.account].income.push(income)
+    }
+    addNewExpense(expense) {
+        this.accounts[expense.account].expenses.push(expense)
+    }
 }
+
+const manager = new Manager()
 
 const expensesAndIncomeElementContainer = document.getElementById('expenses-and-income-container')
 
@@ -51,16 +37,20 @@ expensesAndIncomeElementContainer.addEventListener('submit', (e) => {
     e.preventDefault()
     if(e.target.id == 'expenses-form') {
         const newExpense = getInformationFromInputs('expense')
+        manager.addNewExpense(newExpense)
+        manager.updateView(newExpense.account)
     }
     if(e.target.id == 'income-form') {
         const newIncome = getInformationFromInputs('income')
+        manager.addNewIncome(newIncome)
+        manager.updateView(newIncome.account)
     }
 })
-
+  
 const getInformationFromInputs = (inputsArea) => {
     const date = new Date(Date.now()).toLocaleDateString()
     return {
-        amount: document.getElementById(`${inputsArea}-amount-input`).value,
+        amount: Number(document.getElementById(`${inputsArea}-amount-input`).value),
         account: document.getElementById(`${inputsArea}-account`).selectedOptions[0].label,
         categorie: document.getElementById(`${inputsArea}-categorie`).selectedOptions[0].label,
         comment: document.getElementById(`${inputsArea}-comment-input`).value,
